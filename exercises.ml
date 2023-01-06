de51cf2068358f3831e629c17b2df769fef8058c
Kevin Conner <connerk@gmail.com>
2023-01-05 22:08:35 -0800

    More exercises

--- exercises.ml
+++ exercises.ml
@@ -115,3 +115,114 @@ let rec pack lst =
 ;;
 
 pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"];;
+
+(* Run-length encoding *)
+
+let rec encode = function
+  | [] -> [] 
+  | h :: t -> let e = encode t in begin match e with
+    | [] -> [(1, h)]
+    | (c, i) :: et -> if h = i
+      then (c + 1, i) :: et
+      else (1, h) :: e
+    end
+;;
+
+encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+
+(* Modified run-length encoding *)
+
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+;;
+
+let rec encode = function
+  | [] -> [] 
+  | h :: t -> let e = encode t in begin match e with
+    | [] -> [One h]
+    | One i :: et -> if h = i
+      then Many (2, i) :: et
+      else One h :: e
+    | Many (c, i) :: et -> if h = i
+      then Many (c + 1, i) :: et
+      else One h :: e
+    end
+;;
+
+encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+
+(* Decode a run-length encoded list *)
+
+let rec decode = function
+  | [] -> []
+  | One i :: t -> i :: decode t
+  | Many (c, i) :: t -> if c = 1
+    then i :: decode t
+    else i :: decode (Many (c - 1, i) :: t)
+;;
+
+decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];;
+
+(* Duplicate the elements of a list *)
+let rec duplicate = function
+  | [] -> []
+  | h :: t -> h :: h :: duplicate t
+;;
+
+duplicate ["a"; "b"; "c"; "c"; "d"];;
+
+(* Repeat the elements of a list a given number of times *)
+
+let rec replicate lst n =
+  if n = 1
+    then lst
+    else match lst with
+      | [] -> []
+      | h :: t -> h :: replicate [h] (n - 1) @ replicate t n
+;;
+
+replicate ["a"; "b"; "c"] 3;;
+
+(* Drop every nth element from a list *)
+
+let rec drop_aux lst n c =
+  match lst with
+    | [] -> []
+    | h :: t -> if c = 1
+      then drop_aux t n n
+      else h :: drop_aux t n (c - 1)
+;;
+
+let drop lst n = drop_aux lst n n
+;;
+
+drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;;
+
+(* Split a list into two parts; the length of the first part is given *)
+
+let rec split lst n =
+  if n = 0
+    then ([], lst)
+    else match lst with
+      | [] -> ([], [])
+      | h :: t -> let (b, e) = split t (n - 1) in (h :: b, e)
+;;
+
+split ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;;
+
+split ["a"; "b"; "c"; "d"] 5;;
+
+(* Extract a slice from a list *)
+
+let rec slice lst i k =
+  if k < 0
+    then []
+    else match lst with
+      | [] -> []
+      | h :: t -> if i <= 0
+        then h :: slice t (i - 1) (k - 1)
+        else slice t (i - 1) (k - 1)
+;;
+
+slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6;;
